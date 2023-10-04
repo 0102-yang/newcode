6e0f7a1b2fe679631f59171919aba28f79621214
0102-yang <1657375983@qq.com>
2023-10-05 00:40:00 +0800

    Finish NC66.

---
+++ easy/NC66两个链表的第一个公共结点.cpp
@@ -0,0 +1,76 @@
+/*
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) :
+            val(x), next(NULL) {
+    }
+};*/
+#include <cmath>
+
+struct ListNode
+{
+    int val;
+    struct ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+// Best Solution
+class BestSolution
+{
+public:
+    ListNode* FindFirstCommonNode(ListNode* pHead1, ListNode* pHead2)
+    {
+        ListNode* node1 = pHead1;
+        ListNode* node2 = pHead2;
+        while (node1 != node2) {
+            node1 = !node1 ? pHead2 : node1->next;
+            node2 = !node2 ? pHead1 : node2->next;
+        }
+        return node1;
+    }
+};
+
+// My Solution
+class MySolution
+{
+public:
+    ListNode* FindFirstCommonNode(ListNode* pHead1, ListNode* pHead2)
+    {
+        if (!pHead1 || !pHead2) {
+            return nullptr;
+        }
+
+        int length1 = GetListLength(pHead1);
+        int length2 = GetListLength(pHead2);
+        int steps_to_move = abs(length1 - length2);
+        for (int i = 0; i < steps_to_move; i++) {
+            if (length1 > length2) {
+                pHead1 = pHead1->next;
+            } else {
+                pHead2 = pHead2->next;
+            }
+        }
+
+        while (pHead1) {
+            if (pHead1 == pHead2) {
+                return pHead1;
+            }
+            pHead1 = pHead1->next;
+            pHead2 = pHead2->next;
+        }
+
+        return nullptr;
+    }
+
+private:
+    int GetListLength(ListNode* head)
+    {
+        int length = 0;
+        while (head) {
+            head = head->next;
+            length++;
+        }
+        return length;
+    }
+};
