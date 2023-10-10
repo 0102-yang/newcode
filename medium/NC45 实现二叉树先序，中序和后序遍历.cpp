9e240b03ec5daaeb09fe52389731478e8262125d
0102-yang <1657375983@qq.com>
2023-10-10 18:46:06 +0800

    Finish NC45.

---
+++ medium/NC45 实现二叉树先序，中序和后序遍历.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+/**
+ * struct TreeNode {
+ *	int val;
+ *	struct TreeNode *left;
+ *	struct TreeNode *right;
+ *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * };
+ */
+class Solution
+{
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param root TreeNode类 the root of binary tree
+     * @return int整型vector<vector<>>
+     */
+    vector<vector<int>> threeOrders(TreeNode* root)
+    {
+        vector<vector<int>> orders(3);
+        preOrder(orders[0], root);
+        inOrder(orders[1], root);
+        postOrder(orders[2], root);
+        return orders;
+    }
+
+private:
+    void preOrder(vector<int>& order, TreeNode* root)
+    {
+        if (!root) {
+            return;
+        }
+
+        order.push_back(root->val);
+        preOrder(order, root->left);
+        preOrder(order, root->right);
+    }
+
+    void inOrder(vector<int>& order, TreeNode* root)
+    {
+        if (!root) {
+            return;
+        }
+
+        inOrder(order, root->left);
+        order.push_back(root->val);
+        inOrder(order, root->right);
+    }
+
+    void postOrder(vector<int>& order, TreeNode* root)
+    {
+        if (!root) {
+            return;
+        }
+
+        postOrder(order, root->left);
+        postOrder(order, root->right);
+        order.push_back(root->val);
+    }
+};
