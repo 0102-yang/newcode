9bfcee4abb00598a8985c039b855051290b14717
0102-yang <1657375983@qq.com>
2023-10-17 07:46:24 +0800

    Finish NC9.

---
+++ easy/NC9 二叉树中和为某一值的路径(一).cpp
@@ -0,0 +1,40 @@
+/**
+ * struct TreeNode {
+ *	int val;
+ *	struct TreeNode *left;
+ *	struct TreeNode *right;
+ *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * };
+ */
+struct TreeNode
+{
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param root TreeNode类 
+     * @param sum int整型 
+     * @return bool布尔型
+     */
+    bool hasPathSum(TreeNode* root, int sum)
+    {
+        if (!root) {
+            return false;
+        }
+
+        sum -= root->val;
+        if (!root->left && !root->right && sum == 0) {
+            return true;
+        }
+        return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
+    }
+};
