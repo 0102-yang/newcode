e166d585ea1b3fc8f9ce45bc3aa83844e4ab94a0
0102-yang <1657375983@qq.com>
2023-10-25 14:39:15 +0800

    Finish NC49.

---
+++ challenging/NC49 最长的括号子串.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+
+#include "../common-header.hpp"
+
+// 双指针方法
+class BestSolution
+{
+public:
+    /**
+     * 
+     * @param s string字符串 
+     * @return int整型
+     */
+    int longestValidParentheses(string s)
+    {
+        // write code here
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = max(maxlength, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = (int)s.length() - 1; i >= 0; i--) {
+            if (s[i] == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = max(maxlength, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
+    }
+};
+
+// 动态规划方法
+class MySolution
+{
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param s string字符串 
+     * @return int整型
+     */
+    int longestValidParentheses(string s)
+    {
+        vector<int> dp(s.length(), 0);
+        const int str_length = s.length();
+
+        for (int i = 1; i < str_length; i++) {
+            if (s[i] == ')') {
+                if (s[i - 1] == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] >= 1 && s[i - dp[i - 1] - 1] == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
