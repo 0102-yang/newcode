698b971dea010036c990bbf045ddfa6c4c3a88fc
0102-yang <1657375983@qq.com>
2023-10-25 10:03:00 +0800

    Finish NC48.

---
+++ easy/NC48 在旋转过的有序数组中寻找目标值.cpp
@@ -0,0 +1,52 @@
+#include "../common-header.hpp"
+
+class Solution
+{
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param nums int整型vector 
+     * @param target int整型 
+     * @return int整型
+     */
+    int search(vector<int>& nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int left_num = nums[left];
+            int right_num = nums[right];
+            int mid_num = nums[mid];
+
+            if (mid_num == target) {
+                return mid;
+            }
+            if (right_num == target) {
+                return right;
+            }
+            if (left_num == target) {
+                return left;
+            }
+
+            if (left_num > mid_num) {
+                if (target > mid_num && target < right_num) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (target > left_num && target < mid_num) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
