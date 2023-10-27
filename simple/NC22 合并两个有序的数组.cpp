faafa956d46e2f334684e7c9c7bc24be6cfa5106
0102-yang <1657375983@qq.com>
2023-10-27 17:48:01 +0800

    Finish NC22.

---
+++ simple/NC22 合并两个有序的数组.cpp
@@ -0,0 +1,18 @@
+#include "../common-header.hpp"
+
+class Solution
+{
+public:
+    void merge(int A[], int m, int B[], int n)
+    {
+        int copy_to_index = m + n - 1;
+        int copy_from_a_index = m - 1;
+        int copy_from_b_index = n - 1;
+
+        while (copy_to_index >= 0) {
+            int a = copy_from_a_index >= 0 ? A[copy_from_a_index] : -10000;
+            int b = copy_from_b_index >= 0 ? B[copy_from_b_index] : -10000;
+            A[copy_to_index--] = a < b ? B[copy_from_b_index--] : A[copy_from_a_index--];
+        }
+    }
+};
