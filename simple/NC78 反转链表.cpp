51175bd6ca558cf9c5ae311b6b28b15314583c91
0102-yang <1657375983@qq.com>
2024-03-28 22:29:26 +0800

    Add new solution to NC78.

--- simple/NC78 反转链表.cpp
+++ simple/NC78 反转链表.cpp
@@ -8,7 +8,7 @@
  * };
  */
 
-class Solution
+class Solution1
 {
 public:
     /**
@@ -36,5 +36,30 @@ public:
         }
 
         return head;
+    }
+};
+
+class Solution2
+{
+public:
+    /**
+   * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+   *
+   *
+   * @param head ListNode类
+   * @return ListNode类
+   */
+    ListNode* ReverseList(ListNode* head)
+    {
+        ListNode* new_head = nullptr;
+
+        while (head) {
+            ListNode* tmp = head->next;
+            head->next = new_head;
+            new_head = head;
+            head = tmp;
+        }
+
+        return new_head;
     }
 };
