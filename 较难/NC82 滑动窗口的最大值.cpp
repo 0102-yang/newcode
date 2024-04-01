fbdd36ce3cdb3031334e46c67c8ded810b390901
0102-yang <1657375983@qq.com>
2024-04-01 19:49:34 +0800

    Finish NC82.

---
+++ 较难/NC82 滑动窗口的最大值.cpp
@@ -0,0 +1,36 @@
+#include "../common-header.hpp"
+
+class Solution
+{
+public:
+    vector<int> maxInWindows(vector<int>& num, int size)
+    {
+        vector<int> max_values;
+        if (size == 0 || static_cast<size_t>(size) > num.size()) {
+            return max_values;
+        }
+
+        vector<int> heap;
+        heap.reserve(size);
+        for (size_t i = 0; i < size; i++) {
+            heap.push_back(num[i]);
+        }
+        ranges::make_heap(heap);
+        max_values.push_back(heap.front());
+
+        size_t begin_idx = 0;
+        size_t end_idx = begin_idx + size;
+        while (end_idx < num.size()) {
+            const auto itr = ranges::find(heap, num[begin_idx]);
+            heap.erase(itr);
+            heap.push_back(num[end_idx]);
+            ranges::make_heap(heap);
+            max_values.push_back(heap.front());
+
+            begin_idx++;
+            end_idx++;
+        }
+
+        return max_values;
+    }
+};
